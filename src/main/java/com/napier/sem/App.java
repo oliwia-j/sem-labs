542d8e7fd12629e44edb5d31b04b301dcfa62ab4
oliwia-j <olivska@gmail.com>
2024-02-20 15:42:03 +0000

    Added display functionality for Use Case 01 from labs.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -24,6 +24,7 @@ public class App {
         ArrayList<Employee> employees = database.getAllSalaries();
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
+        database.printSalaries(employees);
 
         // UC-01: first attempt
         // Get all salaries
@@ -197,6 +198,25 @@ public class App {
             return null;
         }
     }
+
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
+
 
     /**
      * Retrieves salary details of all employees from database in ascending order by employees' IDs.
